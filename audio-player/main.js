c0c48d92fe665763560e66f7457132ce4c6dc3d6
Eugen270181 <sadeugen@gmail.com>
2023-09-26 16:06:20 +0300

    feat: feat progress bar & timeline 09/26/2023 16:06:20

--- audio-player/main.js
+++ audio-player/main.js
@@ -2,7 +2,14 @@ const btnPausePlay = document.querySelector('.control-btn-play');;
 const btnPrev = document.querySelector('.control-btn-prev');
 const btnNext = document.querySelector('.control-btn-next');
 const cards = document.querySelectorAll('.slider-card');
-const audio = document.querySelector('#mp3');
+let audio = document.querySelector('#mp3');
+let progressBar = document.querySelector('.time-container-progressBar');
+let lengthTrack = document.querySelector('.time-container-trackTime');
+let currentTime = document.querySelector('.time-container-currentTime');
+let onePercentTrack = 0;
+setInterval(updateCurrentTime, 300);
+
+
 const countCards = cards.length;
 let activeCard=0;
 const trackList = [
@@ -13,11 +20,14 @@ const trackList = [
   
 
 function goTo(index) {
+  progressBar.value = 0;
   cards[activeCard].classList.remove('active');
   if (index===-1) {index=countCards-1}; if (index===countCards) {index=0;};
   cards[index].classList.add('active');
-  if(audio.paused) audio.src = "./assets/audio/"+trackList[index];
-  else {audio.src = "./assets/audio/"+trackList[index]; audio.play();}
+  audio.src = "./assets/audio/"+trackList[index]; //loading metadata
+  progressBar.max = 100;
+  updateCurrentTime(); //update current track time
+  audioPlayPause(); //play audio because default it paused when loaded
   activeCard=index;
 }
 
@@ -26,7 +36,29 @@ function audioPlayPause() {
   else { btnPausePlay.src = "./assets/icon/play.png"; audio.pause();}
 }
 
+function audioProgressBar() {
 
+}
+
+/*-------------------------------------------------------------*/
 btnPrev.addEventListener('click', () => goTo(activeCard-1));
 btnNext.addEventListener('click', () => goTo(activeCard+1));
 btnPausePlay.addEventListener('click', audioPlayPause);
+btnPausePlay.addEventListener('change', audioProgressBar);
+/*---------------------------------------------------------------*/
+audio.onloadedmetadata = function() {
+  onePercentTrack = audio.duration/100;
+  lengthTrack.textContent = formatTime(audio.duration);
+}
+audio.onended = goTo(activeCard+1);
+/*-----------------------------------------------------*/
+function formatTime (time) { //format = M:SS
+  time = Math.floor(time);
+  const minutes = Math.floor(time/60);
+  const seconds = time%60;
+  return `${minutes}:${(seconds<=9)?'0':''}${seconds}`;
+}
+function updateCurrentTime() {
+  currentTime.textContent = formatTime(audio.currentTime);
+}
+
